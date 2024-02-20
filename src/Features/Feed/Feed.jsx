87f9083a815da71d37e359c8b594ea3ec9b9e5e4
aliasger <aliasger@belgiumwebnet.com>
2024-02-20 18:53:54 +0530

    add avatr

--- src/Features/Feed/Feed.jsx
+++ src/Features/Feed/Feed.jsx
@@ -1,5 +1,13 @@
 const Feed = () => {
-  return <div className="bg-white p-4 border rounded-lg"></div>;
+  return (
+    <div className="bg-white p-4 border rounded-lg">
+      <div className="">
+        <img src="https://doodleipsum.com/700/avatar-2?i=29a11c4444c4c0d795d3aba1c0ce37d0" alt="" className="size-14 rounded-full" />
+        <h3 className="text-md">John Doe</h3>
+        <div>@johndoe12</div>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
